4a79380b7cc387c71f65e3f53633cda6fc479d0d
谷口純也 <taniguchi@taniguchijunya-no-iMac.local>
2018-07-01 17:40:58 +0900

    make it better

---
+++ public/assets/cards-a2553240d15e85a8f7567929a6d2d6262656661d171237d10dc039d1d5f0ecb7.js
@@ -0,0 +1,1 @@
+(function(){window.delData=function(n,t){if(confirm("\u300c"+t+"\u300d\u306e\u30c7\u30fc\u30bf\u3092\u524a\u9664\u3057\u307e\u3059\u304b\uff1f"))return document.location="/cards/delete/"+n,!1}}).call(this);
